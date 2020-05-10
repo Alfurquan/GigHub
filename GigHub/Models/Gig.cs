d54b52a9a74c2b0b5804fe42cbcddb05d8e14102
Alfurquan Zahedi <Alfurquan.Zahedi@kantar.com>
2020-05-10 20:22:08 +0530

    commit

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -6,6 +6,9 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
+
+        public bool IsCancelled { get; set; }
+
         public ApplicationUser Artist { get; set; }
         [Required]
         public string ArtistId { get; set; }
