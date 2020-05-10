512ab2aac0cc89c0f8693f41b33be8da1d58974b
Alfurquan Zahedi <Alfurquan.Zahedi@kantar.com>
2020-05-10 16:38:16 +0530

    Added Artist I am following

---
+++ GigHub/Controllers/FolloweesController.cs
@@ -0,0 +1,26 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public FolloweesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var followees = context.Followings.Where(f => f.FollowerId == userId).Include(f => f.Followee).ToList();
+            return View(followees);
+        }
+    }
+}
