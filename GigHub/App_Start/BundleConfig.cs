42f444e80d82fe9b80e7c3a39e5bd5a9aefcd590
Alfurquan Zahedi <Alfurquan.Zahedi@kantar.com>
2020-05-13 13:00:00 +0530

    Modularized JS using module revealing pattern

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -7,6 +7,14 @@ namespace GigHub
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/App/Services/AttendanceService.js",
+                        "~/Scripts/App/Services/FollowingService.js",
+                        "~/Scripts/App/Controllers/GigsDetailController.js",
+                        "~/Scripts/App/Controllers/GigsController.js",
+                        "~/Scripts/App/app.js"
+                        ));
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
---
+++ GigHub/Scripts/App/Controllers/GigsDetailController.js
@@ -0,0 +1,38 @@
+﻿
+var GigsDetailController = function (followingService) {
+
+    var followButton;
+
+    var init = function () {
+        $('.js-toggle-follow').click(toggleFollowings)
+    }
+
+    var toggleFollowings = function (e) {
+        followButton = $(e.target);
+
+        var followeeId = followButton.attr('data-user-id');
+
+        if (followButton.hasClass("btn-default"))
+            followingService.followArtist(followeeId, done, fail);
+        else
+            followingService.unfollowArtist(followeeId, done, fail)
+    }
+
+
+
+    var done = function () {
+        var text = (followButton.text() == "Follow") ? "Following" : "Follow";
+        followButton.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    }
+
+    var fail = function () {
+        alert('Something failed!')
+    }
+
+    return {
+        init: init
+    }
+
+}(FollowingService);
+
+
